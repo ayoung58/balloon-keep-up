848ad576219b8bef351afc3030167f5f362e29af
Theinterna <yalvin58@gmail.com>
2024-08-24 15:34:10 -0700

    Add basic movement code for player 1
    This uses AddForce to "push" the player. The default forcemode is used here. "Translating" the player is expensive with a rigidbody, so forces are used. Time.deltaTime is not used because physics forces are independent of time.

--- Balloon Keep Up/Assets/Scripts/Player1Controller.cs
+++ Balloon Keep Up/Assets/Scripts/Player1Controller.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector3.forward * speed * forwardInput);
+        rb.AddForce(Vector3.right * speed * horizontalInput);
     }
 }
