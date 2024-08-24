32c0b0a6e3b8d73e2657f70108dde824743faf74
Theinterna <yalvin58@gmail.com>
2024-08-24 16:44:03 -0700

    Implement keep up ability for player 2
    Very similar to player 1, will be thinking about inheritance afterwards.

--- Balloon Keep Up/Assets/Scripts/Player2Controller.cs
+++ Balloon Keep Up/Assets/Scripts/Player2Controller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player2Controller : MonoBehaviour
 {
     public float speed = 1;
+    public float keepUpStrength = 5;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,14 @@ public class Player2Controller : MonoBehaviour
         //Apply the movement vector to the current position, which is
         //multiplied by deltaTime and speed for a smooth MovePosition
         rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
+
+    }
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Balloon")) {
+            Rigidbody balloonRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 keepUpDirection = collision.gameObject.transform.position - transform.position;
+            
+            balloonRb.AddForce(keepUpDirection * keepUpStrength, ForceMode.Impulse);
+        }
     }
 }
