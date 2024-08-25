45cc363ca6591d5a57a195f0da83d707c80b03e1
Theinterna <yalvin58@gmail.com>
2024-08-24 21:17:26 -0700

    Change movement code to be more efficient

--- Balloon Keep Up/Assets/Scripts/Player1Controller.cs
+++ Balloon Keep Up/Assets/Scripts/Player1Controller.cs
@@ -19,19 +19,23 @@ public class Player1Controller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
 
-        // Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime * horizontalInput);
-        // rb.MoveRotation(rb.rotation * deltaRotation);
-        // rb.(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
-        float turn = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, turn * rotationSpeed * Time.deltaTime);
-        Vector3 direction = (focalPoint.transform.position - transform.position).normalized;
+        float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
 
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
-        } else if (Input.GetKey(KeyCode.DownArrow)) {
-            rb.MovePosition(transform.position - direction * Time.deltaTime * speed);
-        }
+        // float turn = Input.GetAxis("Horizontal");
+        // transform.Rotate(Vector3.up, turn * rotationSpeed * Time.deltaTime);
+        // Vector3 direction = (focalPoint.transform.position - transform.position).normalized;
+
+        // if (Input.GetKey(KeyCode.UpArrow)) {
+        //     rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
+        // } else if (Input.GetKey(KeyCode.DownArrow)) {
+        //     rb.MovePosition(transform.position - direction * Time.deltaTime * speed);
+        // }
         
     }
 
--- Balloon Keep Up/Assets/Scripts/Player2Controller.cs
+++ Balloon Keep Up/Assets/Scripts/Player2Controller.cs
@@ -7,27 +7,35 @@ public class Player2Controller : MonoBehaviour
     public float speed = 1;
     public float keepUpStrength = 5;
     public float rotationSpeed = 30;
-    private GameObject focalPoint;
+    // private GameObject focalPoint;
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        focalPoint = GameObject.Find("Focal Point 2");
+        // focalPoint = GameObject.Find("Focal Point 2");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float turn = Input.GetAxis("Horizontal2");
-        transform.Rotate(Vector3.up, turn * rotationSpeed * Time.deltaTime);
-        Vector3 direction = (focalPoint.transform.position - transform.position).normalized;
+        float moveVertical = Input.GetAxis("Vertical2");
+        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
 
-        if (Input.GetKey(KeyCode.W)) {
-            rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
-        } else if (Input.GetKey(KeyCode.S)) {
-            rb.MovePosition(transform.position - direction * Time.deltaTime * speed);
-        }
+        float turn = Input.GetAxis("Horizontal2") * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+
+        // float turn = Input.GetAxis("Horizontal2");
+        // transform.Rotate(Vector3.up, turn * rotationSpeed * Time.deltaTime);
+        // Vector3 direction = (focalPoint.transform.position - transform.position).normalized;
+
+        // if (Input.GetKey(KeyCode.W)) {
+        //     rb.MovePosition(transform.position + direction * Time.deltaTime * speed);
+        // } else if (Input.GetKey(KeyCode.S)) {
+        //     rb.MovePosition(transform.position - direction * Time.deltaTime * speed);
+        // }
     }
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Balloon")) {
